fe4c96ecdf4a02eb5802fe431d386f4421948344
doryngal <daryn1.dd@gmail.com>
2023-08-25 14:19:09 +0600

    added: user controller and todo, service and dto.

--- src/main/java/com/todo/todolist/repository/TodoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.todo.todolist.repository;
-
-import com.todo.todolist.model.Todo;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface TodoRepository extends JpaRepository<Todo, Long>{
-
-}
